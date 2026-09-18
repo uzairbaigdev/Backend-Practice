5d75ebb939168f24fd77151e06a42b5fc3e356a3
uzairBaig395 <mirzauzair448@gmail.com>
2026-09-18 12:31:00 +0500

    middleware integerated

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ import { fileURLToPath } from 'node:url'
 
 const app = express()
 const port = 3000
-const publicPath = path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
-
-app.use(express.static(publicPath))
+// const publicPath = path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
+  
+// app.use(express.static(publicPath))
 
 app.use('/users', userRoutes)
 app.use('/products', productRoutes)
+
+let age = 15;
+
+//middleware 1
+const middleware1 = (req,res,next) => {
+   if(age >= 18) {
+      res.send("You are allowed to access this route")
+      next();
+   } else {
+      res.status(403).send("You are not allowed to access this route")
+   }
+}
+
+app.get("/",middleware1,(req,res)=> {
+  res.send("Hello from server.js")
+})
 
 app.listen(port, () => {
   console.log(`app is listening on port number ${port}`)
